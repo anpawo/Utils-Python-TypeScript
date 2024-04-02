4edd4881274709305998b11eb52ca4c66e58d6be
marius rousset <rousset.marius13@gmail.com>
2024-04-02 17:29:15 +0200

    feat: print binary

--- PrintBinary.c
+++ PrintBinary.c
@@ -5,22 +5,20 @@ each byte is space separated
 
 #include <stdio.h>
 
-void print_binary(int x)
-{
-    int shift = sizeof(x) * 8 - 1;
+void print_binary(int x) {
+  int shift = sizeof(x) * 8 - 1;
 
-    do {
-        printf("%d", ((x >> shift) & 1));
-        if (shift % 8 == 0) { // space at each byte
-            printf(" ");
-        }
-    } while (shift--);
-    printf("\n");
+  do {
+    printf("%d", ((x >> shift) & 1));
+    if (shift % 8 == 0) { // space at each byte
+      printf(" ");
+    }
+  } while (shift--);
+  printf("\n");
 }
 
-int main(void)
-{
-    int x = 5;
+int main(void) {
+  int x = 'a';
 
-    print_binary(x);
+  print_binary(x);
 }
